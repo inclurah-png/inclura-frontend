d8f32a8322db1fd490c65415480a79ced5c61983
inclurah-png <inclurah@gmail.com>
2026-05-16 22:39:11 +0100

    Update app.js
    Extended description

--- app.js
+++ app.js
@@ -1,4 +1,48 @@
 ```js
-alert("JavaScript is now working");
+console.log("APP STARTED");
+
+alert("JavaScript connected successfully");
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const loginBtn =
+    document.getElementById("loginBtn");
+
+  const logoutBtn =
+    document.getElementById("logoutBtn");
+
+  const saveProfileBtn =
+    document.getElementById("saveProfileBtn");
+
+  if (loginBtn) {
+
+    loginBtn.addEventListener("click", () => {
+
+      alert("Login button works");
+    });
+  }
+
+  if (logoutBtn) {
+
+    logoutBtn.addEventListener("click", () => {
+
+      alert("Logout button works");
+    });
+  }
+
+  if (saveProfileBtn) {
+
+    saveProfileBtn.addEventListener("click", () => {
+
+      alert("Save profile button works");
+    });
+  }
+});
+
+window.createPost = function () {
+
+  alert("Post button works");
+};
 ```
+
 
