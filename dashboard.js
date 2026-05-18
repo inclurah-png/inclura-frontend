16d2097d26c9e10b5aeeb1c3bceb98a37148de16
inclurah-png <inclurah@gmail.com>
2026-05-18 13:09:50 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,31 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
+const menuToggle = document.getElementById("menuToggle");
 
-  const menuToggle = document.getElementById("menuToggle");
-  const sidebar = document.getElementById("sidebar");
+const sidebar = document.getElementById("sidebar");
 
-  if(menuToggle && sidebar){
+menuToggle.addEventListener("click", () => {
 
-    menuToggle.addEventListener("click", () => {
-      sidebar.classList.toggle("show-sidebar");
-    });
-
-  }
-
-
-  const cards = document.querySelectorAll(".quick-card");
-
-  cards.forEach(card => {
-
-    card.addEventListener("click", () => {
-
-      const link = card.getAttribute("data-link");
-
-      if(link){
-        window.location.href = link;
-      }
-
-    });
-
-  });
+  sidebar.classList.toggle("show-sidebar");
 
 });
